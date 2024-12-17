51762d24d61b7f887cf27a6d45c743c7ee77d949
ls-ron <rony24th@gmail.com>
2024-12-18 09:52:05 +1300

    changed py file so that project is simpler

--- main.py
+++ main.py
@@ -1,37 +1,29 @@
 from pytube import YouTube, Stream, StreamQuery
-from pytube.exceptions import VideoUnavailable
-import keyboard
-import os
-import ffmpeg
+#import keyboard
+#import os
+#import ffmpeg
 
-
-
+# keyboard.add_hotkey('esc', exit)
 print("This is a YouTube video downloader program. To continue, ", end="")
 print("please input your chosen URL. You will then be provided ", end="")
 print("with a video or audio option to pick from.")
-print("Once that is done, the file will be found in 'Downloads'.")
+print("Once that is done, the file will be found in this folder.")
 print("To close the program, press the 'esc' key")
 
 while True:
-    while True:
-        try:
-            url = input("Enter your YouTube URL here: ")
-            yt = YouTube(url)
-        except VideoUnavailable:
-            print("Video is unavailable, try again")
-        else:
-            break  
-    print(f"You inputted: {yt.title}")
+    url = input("Enter your YouTube URL here: ")
+    yt = YouTube(url)
     while True:
         audio_or_video = input("Enter audio/video for your preferred videotype: ")
         if audio_or_video in ["audio", "video"]:
             break
         else:
             audio_or_video = input("Try again: ")
-    
+    if audio_or_video == "audio":
+        stream = yt.streams.get_audio_only()
+    else:
+        stream = yt.streams.get_highest_resolution()
+    stream.download()
 
     
-
     
-keyboard.wait('esc')
-print("Exited program")
